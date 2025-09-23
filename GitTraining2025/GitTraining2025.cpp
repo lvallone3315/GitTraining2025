eab58f582568de33d6a3ff9e7fb3fdbc014510bd
lvall <lvall@LEE2025>
2025-09-23 12:56:48 -0700

    Add project files.

---
+++ GitTraining2025/GitTraining2025.cpp
@@ -0,0 +1,10 @@
+// GitTraining2025.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello Github!\n";
+}
+
